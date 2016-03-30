f43cfd5f8e398e03dc19854278907d4b7e37c7b8
Ivan <itruf@werbary.ru>
2016-03-30 20:10:34 +0300

    BOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,6 @@
 console.log('This would be the main JS file.');
 
+//JS samples
 function winop()
 {
 windop = window.open("http://www.google.ru","GOOGLE","height=120,width=300");
@@ -8,4 +9,14 @@ windop = window.open("http://www.google.ru","GOOGLE","height=120,width=300");
 function wincl() 
 {
 windop.close();
+}
+
+//BOM samples
+function showAgent()
+{
+	alert(navigator.userAgent);
+}
+function showHref()
+{
+	alert(location.href);
 }
