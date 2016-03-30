f0e61e6db08f2509dd7137fa7b8e1ed6405bb59e
Ivan <itruf@werbary.ru>
2016-03-30 20:45:03 +0300

    Final improvs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,44 @@
-console.log('This would be the main JS file.');
-
 //JS samples
-function winop()
-{
-windop = window.open("http://www.google.ru","GOOGLE","height=120,width=300");
+function winop() {
+    windop = window.open("http://www.google.ru", "GOOGLE", "height=120,width=300");
 }
 
-function wincl() 
-{
-windop.close();
+function wincl() {
+    windop.close();
 }
 
 //BOM samples
-function showAgent()
-{
-	alert(navigator.userAgent);
+function showAgent() {
+    alert(navigator.userAgent);
 }
-function showHref()
-{
-	alert(location.href);
+
+function showHref() {
+    alert(location.href);
 }
+
+//DOM samples
+function makeBGRed() {
+    if (document.body.style.background.valueOf() === new String("red").valueOf()) {
+        document.body.style.background = "";
+    } else {
+        document.body.style.background = "red";
+    }
+}
+
+function showSectionsCount() {
+    alert(document.getElementsByTagName('section').length);
+}
+
+//jQuery
+
+$(document).ready(function() {
+
+    $("#hideable_btn").click(function() {
+        console.log('CLICKED');
+        $(this).hide();
+    });
+
+    $("#shake_btn").click(function() {
+        $(this).effect("shake");
+    });
+});
