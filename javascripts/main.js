54390de1e42d7a272e9a6eb3b27845a2dfd87796
Ivan <itruf@werbary.ru>
2016-03-30 19:39:22 +0300

    JS Sample

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
 console.log('This would be the main JS file.');
+
+function winop()
+{
+windop = window.open("http://www.google.ru","GOOGLE","height=120,width=300");
+}
+
+function wincl() 
+{
+windop.close();
+}
